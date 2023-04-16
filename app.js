f0b9c1f1992009297320b1efed956593e5a76cdd
SamDaQueen <samreenreyaz@outlook.com>
2023-04-16 13:07:24 -0400

    Remove log message

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
 import ExercisesController from "./controllers/exercise/exercises-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
-import mongoose from "mongoose";
 
 // const CONNECTION_STRING =
 //   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/fitlog";
@@ -10,8 +10,6 @@ import mongoose from "mongoose";
 const CONNECTION_STRING =
   "mongodb+srv://fitlog-username:fitlog-password@fitlog.tx1t84a.mongodb.net/Fitlog?retryWrites=true&w=majority";
 mongoose.connect(CONNECTION_STRING);
-
-console.log("DB_CONNECTION_STRING: " + CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
